d10feb66c52a72642ab9b206cdb264b8ca4b3b69
jokoul <74833985+jokoul@users.noreply.github.com>
2022-07-22 13:27:20 +0200

    initialization

---
+++ README.md
@@ -0,0 +1,33 @@
+# project-36-fulele-blog
+
+## Description
+
+This is a blog website made with Vue.js and firebase. In this project, I used the composition API
+
+## Stack
+
+Vue.js, Firebase
+
+## Launch
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    HOME
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  setup() {
+    //to use composition API
+    console.log("setup");
+
+    let name = "mario";
+    let age = 30;
+
+    const handleClick = () => {
+      console.log("you clicked me");
+    };
+
+    return { name, age, handleClick };
+  },
+  data() {
+    return {
+      age: 40,
+    };
+  },
+};
+</script>
