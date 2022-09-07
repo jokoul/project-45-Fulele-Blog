3d828ea1b209c9a8432c1dee1770f152ced51719
jokoul <74833985+jokoul@users.noreply.github.com>
2022-09-07 22:21:00 +0200

    end of card section

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,12 @@
 </template>
 
 <script>
-import Navigation from './components/Navigation'
-import FooterVue from './components/Footer.vue'
+import Navigation from "./components/Navigation";
+import FooterVue from "./components/Footer.vue";
 
 export default {
   name: "app",
-  components: {Navigation, FooterVue},
+  components: { Navigation, FooterVue },
   data() {
     return {};
   },
@@ -43,6 +43,10 @@ export default {
   min-height: 100vh;
 }
 
+main {
+  min-height: 100vh;
+}
+
 .container {
   max-width: 1440px;
   margin: 0 auto;
@@ -59,7 +63,42 @@ export default {
   color: #fff;
 }
 
-main {
-  min-height: 100vh;
+.arrow {
+  margin-left: 8px;
+  width: 12px;
+  path {
+    fill: #fff;
+  }
+}
+
+.arrow-light {
+  path {
+    fill: #000;
+  }
+}
+
+.blog-card-wrap{
+  position:relative;
+  padding:80px 16px;
+  background-color: #f1f1f1;
+  @media(min-width:500px){
+    padding: 100px 16px;
+  }
+
+  .blog-cards{
+    display:grid;
+    gap:32px;
+    grid-template-columns:1fr;
+
+    @media(min-width:500px){
+      grid-template-columns: repeat(2, 1fr);
+    }
+    @media(min-width:900px){
+      grid-template-columns: repeat(3, 1fr);
+    }
+    @media(min-width:1200px){
+      grid-template-columns: repeat(4, 1fr);
+    }
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="home">
     <BlogPost :post="welcomeScreen" />
-    <BlogPost v-for="(post, index) in sampleBlogPost" :post="post" :key="index" />
+    <BlogPost
+      v-for="(post, index) in sampleBlogPost"
+      :post="post"
+      :key="index"
+    />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCards v-for="(post, index) in sampleBlogCards" :post="post" :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import BlogPost from '../components/BlogPost.vue'
+import BlogPost from "../components/BlogPost.vue";
+import BlogCards from "../components/BlogCard.vue";
 
 export default {
   name: "Home",
-  components: {BlogPost},
+  components: { BlogPost, BlogCards },
   data() {
     return {
       welcomeScreen: {
@@ -18,23 +31,55 @@ export default {
         blogPost:
           "Weekly blog articles about medicinal plants used in traditional medicine practices since prehistoric times. Plants synthesize hundreds of chemical compounds for functions including defense against insects, fungi, diseases. Register today to receive new post.",
         welcomeScreen: true,
-        photo: "herbs"  
+        photo: "herbs",
       },
       sampleBlogPost: [
         {
           title: "TURMERIC, a spice with promising medicinal properties",
-          blogHTML:"Turmeric is traditionally used to treat various gastrointestinal disorders, such as inflammation and stomach ulcers.",
-          blogCoverPhoto: "turmeric"
+          blogHTML:
+            "Turmeric is traditionally used to treat various gastrointestinal disorders, such as inflammation and stomach ulcers.",
+          blogCoverPhoto: "turmeric",
         },
         {
           title: "CHLORELLA: to eliminate heavy metals in the body",
-          blogHTML:"Chlorella is recognized as the most effective natural product for naturally eliminating heavy metals from the body.",
-          blogCoverPhoto: "chlorella"
+          blogHTML:
+            "Chlorella is recognized as the most effective natural product for naturally eliminating heavy metals from the body.",
+          blogCoverPhoto: "chlorella",
         },
-      ]
+      ],
+      sampleBlogCards: [
+        {
+          blogTitle: "Blog Card #1",
+          blogCoverPhoto: "stock-1",
+          blogDate: "May 1, 2021",
+        },
+        {
+          blogTitle: "Blog Card #2",
+          blogCoverPhoto: "stock-2",
+          blogDate: "May 2, 2021",
+        },
+        {
+          blogTitle: "Blog Card #3",
+          blogCoverPhoto: "stock-3",
+          blogDate: "May 3, 2021",
+        },
+        {
+          blogTitle: "Blog Card #4",
+          blogCoverPhoto: "stock-4",
+          blogDate: "May 4, 2021",
+        },
+      ],
     };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .blog-card-wrap {
+    h3{
+      font-weight: 300;
+      font-size: 28px;
+      margin-bottom: 32px;
+    }
+  }
+</style>
